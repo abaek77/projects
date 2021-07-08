ad2ff40f133fcb92b31b0b210f19b04844c560a0
abaek77 <83965835+abaek77@users.noreply.github.com>
2021-07-08 16:12:27 -0400

    Update form.js

--- todolist REACT/src/form.js
+++ todolist REACT/src/form.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import './App.css';
 
 function Form ({setTodoinput, setTodos, todoinput, todos}) {
+    
     function Savetodoinput(event) {
         console.log(event.target.value);
         setTodoinput(event.target.value);
     };
 
-    const Objectifytodo = (event) => {
-        event.preventDefault()
-        setTodos([...todos, {text: todoinput, completed: false}])
-        console.log({todos})
-    }
+
+    function Objectifytodo (event) {
+        event.preventDefault();
+        setTodos([...todos, {text: todoinput, completed: false, id: Math.random()*1000000}]);
+        console.log({todos});
+    };
+
 
     return(
         <form>
             <input type="text" className="inputtext" onChange = {Savetodoinput}/>
-            <button onClick={Objectifytodo}>+</button>
+            <button onClick={Objectifytodo} className="firstbutton">+</button>
         </form>
     );
         
