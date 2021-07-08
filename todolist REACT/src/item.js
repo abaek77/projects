5cf33918be2fed8f3f0b3c887a712442b488ab4c
abaek77 <83965835+abaek77@users.noreply.github.com>
2021-07-08 16:12:48 -0400

    Update item.js

--- todolist REACT/src/item.js
+++ todolist REACT/src/item.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import List from './list';
+import './App.css';
 
-function Item({text}){
+
+function Item({text, setTodos, todos, todo}){
+
+    function deletefromlist () {
+        console.log({todo})
+        setTodos(todos.filter((todoitem) => todoitem.id !== todo.id))
+        //  if(todos=[""]){
+        //      alert("nice job!")
+        //                  }
+    }
+
     return(
         <div className="item">
         <li>{text}</li>
-        <li>sdlkfjsdklfj</li>
-        <button>X</button>
+        <button onClick = {deletefromlist}>Complete</button>
     </div>
     );
 
