9735c290f978d808d73b51135ea01f016cc63af4
abaek77 <83965835+abaek77@users.noreply.github.com>
2021-07-12 15:41:03 -0400

    Update Form.js

--- converter/src/Form.js
+++ converter/src/Form.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import {debounce} from "lodash";
+import {useDebounce} from "use-debounce";
 
 function Form () {
     
     const [unit, setUnit] = useState("____");
     const [numInput, setNumInput] = useState(1);
+    const [numResult] = useDebounce(numInput, 400);
 
     const handleChangeUnit = ({target: {value}}) => {
         setUnit(value);
@@ -14,28 +15,29 @@ function Form () {
         setNumInput(value);
     }
 
-    const generateResult = () => {
+    const generateResult =() => {
     
         switch(unit){
             case "USD":
-                return <h4>Converted value from USD to PLN is {numInput*3.829386}</h4>
+                return <h4>Converted value from USD to PLN is {numResult*3.829386}</h4>
             case "miles":
-                return <h4>Converted value from miles to kilometers is {numInput*1.60934}</h4>
+                return <h4>Converted value from miles to kilometers is {numResult*1.60934}</h4>
             case "Fahrenheit":
-                return <h4>Converted value from Fahrenheit to Celsius is {(numInput-32)/1.8}</h4>
+                return <h4>Converted value from Fahrenheit to Celsius is {(numResult-32)/1.8}</h4>
         }
     }
 
+
     return(
         <form>
-            <select value = {unit} onChange={ handleChangeUnit}>
+            <select value = {unit} onChange={handleChangeUnit}>
                 <option hidden={true}>select</option>
                 <option >miles</option>
                 <option >USD</option>
                 <option >Fahrenheit</option>
             </select>
             <input type="number" value={numInput} onChange={handleChangeNum}/>
-            {unit && numInput ?
+            {unit && numResult ?
                 generateResult()
                 :
                 <h4>Provide some value.</h4>
