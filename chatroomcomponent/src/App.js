677d3d96784f21bb83853374bda3fa54cbf88019
Alex Baek <alex.baek@evemeta.com>
2021-07-20 13:29:21 -0400

    flexible component

---
+++ chatroomcomponent/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Component from './flexiblecomponent';
+
+function App() {
+  
+  
+  
+  return (
+    <div className="App">
+      <Component category="Comedy" people="4/8" title="Watch stream together group - randoms"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ chatroomcomponent/src/flexiblecomponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Component (props) {
+return(
+<form>
+<p className="title">{props.title}</p>
+<p className="category-title">Category</p>
+<p className="category">{props.category}</p>
+<img src="https://static.thenounproject.com/png/160062-200.png" alt="peopleicon" />
+<p className="people">{props.people}</p>
+</form>
+)
+}
+
+export default Component;
