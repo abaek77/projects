d7c3913265ca78d925adcec0b589e549b256e2d1
Alex Baek <alex.baek@evemeta.com>
2021-07-20 14:36:22 -0400

    .

--- chatroomcomponent/src/flexiblecomponent.js
+++ chatroomcomponent/src/flexiblecomponent.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Component (props) {
 return(
-<form>
-<p className="title">{props.title}</p>
+<div className="component"><p className="title">{props.title}</p>
 <p className="category-title">Category</p>
 <p className="category">{props.category}</p>
 <img src="https://static.thenounproject.com/png/160062-200.png" alt="peopleicon" />
-<p className="people">{props.people}</p>
-</form>
+<p className="people">{props.people}</p></div>
+
+
 )
 }
 
