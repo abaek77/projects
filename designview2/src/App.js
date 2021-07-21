0f690138f6e854062f1d1a82365e6bb445a51a46
Alex Baek <alex.baek@evemeta.com>
2021-07-20 22:46:23 -0400

    design2 finished

--- designview2/src/App.js
+++ designview2/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+      
+          <h2><a href="" className="bento">bento</a></h2>
+          <select>
+          <option>Discover</option>
+        </select>
+          <a href="" className="navlinks">Premade template</a>
+          <a href="" className="navlinks">Generator</a>
+      <div className="navright">
+          <button className="createbutton">Create +</button>
+          <button className="signinbutton">Sign In</button>
+      </div>
+      </div>
+        
+      <div className="main">
+      <h1>illustration builder</h1>
+      <p className="description">Easily create and publish 3D web experiences. Build and iterate fast with production-ready results.</p>
+      <form>
+        <input placeholder="Your email" />
+        <button className="getstarted">Get started</button>
+      </form>
+      </div>
+      <div className="bottom">
+        <p className="scrolldown">Scroll down to learn more</p>
+      </div>
+
     </div>
+
   );
 }
 
