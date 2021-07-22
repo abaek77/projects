19cd3fbfd0b72aa7415ea767ec2dc2f331441d53
Alex Baek <alex.baek@evemeta.com>
2021-07-22 14:30:58 -0400

    .

--- designview2/src/App.js
+++ designview2/src/App.js
@@ -6,13 +6,14 @@ function App() {
     <div className="border">
     <div className="App">
       <div className="nav">
-      
+      <div className="navleft">
           <h2><a href="" className="bento">bento</a></h2>
           <select>
           <option>Discover</option>
-        </select>
+          </select>
           <a href="" className="navlinks">Premade template</a>
           <a href="" className="navlinks">Generator</a>
+      </div>
       <div className="navright">
           <button className="createbutton">Create +</button>
           <button className="signinbutton">Sign In</button>
@@ -22,10 +23,10 @@ function App() {
       <div className="main">
       <h1>illustration builder</h1>
       <p className="description">Easily create and publish 3D web experiences. Build and iterate fast with production-ready results.</p>
-      <form>
+      <div className="form">
         <input placeholder="Your email" />
         <button className="getstarted">Get started</button>
-      </form>
+      </div>
       </div>
       <div className="bottom">
         <p className="scrolldown">Scroll down to learn more</p>
