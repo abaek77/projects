462066db83aad67aa51e17e885723fe7c9722662
Alex Baek <alex.baek@evemeta.com>
2021-07-22 01:24:32 -0400

    .

--- designview3/src/App.js
+++ designview3/src/App.js
@@ -6,72 +6,90 @@ function App() {
   return (
     <div className="App">
       <div className="nav">
-        <h3>FLO.</h3>
+      <a className="title"href=""><h3 className="leftside" >FLO.</h3></a>
         <div className="navright">
             <nav>
-              <a href="">New Arrival</a>
-              <a href="">Best Seller</a>
-              <a href="">Reviews</a>
+              <a className="navlinks"href="">New Arrival</a>
+              <a className="navlinks"href="">Best Seller</a>
+              <a className="navlinks"href="">Reviews</a>
             </nav>
             <button className="signup">Sign Up</button>
         </div>
       </div>
+
+
       <div className="topmain">
         <h1>Gift and Flower for Your Beloved</h1>
-        <p>Priding ourselves on the quality, bold colours and sustainability of our products, will give you a boost of confidence.</p>
+        <p className="description">Priding ourselves on the quality, bold colours and sustainability of our products, will give you a boost of confidence.</p>
         <button className="startshopping">Start Shopping</button>
-        <button className="package">
-          <img src="" alt="" />
-          <h3>Tulip Package</h3>
-          <h2>$70.99</h2>
-          <p>Detail --)</p>
+        <div className="buttons1">
+        <button id="tulip1"className="package">
+          <img src="https://flowersgifts-ua.com/wp-content/uploads/2018/03/11-tulips.png" alt="tulipbouquet" className="tulipbouquet"/>
+          <div className="inside">
+          <h3 className="flowerpackage">Tulip Package</h3>
+          <h2 className="flowerprice">$70.99</h2>
+          <p className="detail">Detail --)</p>
+          </div>
         </button>
-        <button className="package">
-          <img src="" alt="" />
-          <h3>Rose Package</h3>
-          <h2>$70.99</h2>
-          <p>Detail --)</p>
+        <button id="rose1"className="package">
+          <img src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Roses-PNG/Transparent_Red_Roses_Bouquet_PNG_Clipart_Picture.png?m=1434276736" alt="rosebouquet" className="rosebouquet" />
+          <div className="inside">
+          <h3 className="flowerpackage">Rose Package</h3>
+          <h2 className="flowerprice">$70.99</h2>
+          <p className="detail">Detail --)</p>
+          </div>
         </button>
-        <button className="package"></button>
+        </div>
       </div>
+
+
       <div className="midbar">
       <div className="midbar1">
-        <h2>3000+</h2>
-        <h2>5000+</h2>
-        <h2>7000+</h2>
-        <h2>25+</h2>
+        <h2 className="number">3000+</h2>
+        <h2 className="number">5000+</h2>
+        <h2 className="number">7000+</h2>
+        <h2 className="number">25+</h2>
       </div>
       <div className="midbar2">
-        <h4>Packages Sold</h4>
-        <h4>Bouquet Sold</h4>
-        <h4>Happy Clients</h4>
-        <h4>Years of Experience</h4>
+        <h4 className="packagessold">Packages Sold</h4>
+        <h4 className="bouquetsold">Bouquet Sold</h4>
+        <h4 className="happyclients">Happy Clients</h4>
+        <h4 className="yearsof">Years of Experience</h4>
       </div>
       </div>
+      
+      
       <div className="bottommain">
         <div className="bottomnav">
           <p>Product</p>
-          <button>)</button>
-          <h3>Our Best Sellers This Month</h3>
+          <div className="bottombutton">
+          <button className="bottomleft">(</button>
+          <button className="bottomright">)</button>
+            </div>
         </div>
+        <h3 className="title2">Our Best Sellers This Month</h3>
         <div className="bestsellers">
           <button id="one"className="seller">
             <div className="flag">
-            <h3>New!</h3>
-            </div>
+            <h3 className="new">New!</h3>
             <button className="heart"></button>
-            <div>
-              <p>for special person</p>
-              <h3>Aster Bouquet</h3>
-              <h2>$ 27.23</h2>
+            </div>
+            
+            <div className="sellerbottom">
+              <p className="specialnew">for special person</p>
+              <h3 className="aster">Aster Bouquet</h3>
+              <h2 className="astercost">$ 27.23</h2>
+              <div className="cartspacing">
               <button className="cart"></button>
+              </div>
             </div>
           </button>
           <button id="two"className="seller">
             <div className="flag">
             <h3>New!</h3>
-            </div>
             <button className="heart"></button>
+            </div>
+            
             <div>
               <p>for special person</p>
               <h3>Rose Bouquet</h3>
