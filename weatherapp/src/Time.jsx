e4352993aa4d4cdc8216b72272fc97edbdc61cd6
Alex Baek <alex.baek@evemeta.com>
2021-08-04 12:01:30 -0400

    time should update now

--- weatherapp/src/Time.jsx
+++ weatherapp/src/Time.jsx
@@ -6,33 +6,35 @@ function Time({ minutes, hours }) {
     const [currentTime, setCurrentTime] = useState("")
     useEffect(() => {
 
-        if (hours === 0) {
-            if (minutes < 10) {
-                setCurrentTime(hours + 12 + ":" + minutes.padStart(2, 0))
+        const interval = setInterval(() => {
+            if (hours === 0) {
+                if (minutes < 10) {
+                    setCurrentTime(hours + 12 + ":" + minutes.padStart(2, 0))
+                }
+                else {
+                    setCurrentTime(hours + 12 + ":" + minutes)
+                }
+            }
+            else if (hours > 13) {
+                if (minutes < 10) {
+                    setCurrentTime(hours - 12 + ":" + minutes.padStart(2, 0))
+                }
+                else {
+                    setCurrentTime(hours - 12 + ":" + minutes)
+                }
             }
             else {
-                setCurrentTime(hours + 12 + ":" + minutes)
+                if (minutes < 10) {
+                    setCurrentTime(hours + ":" + minutes.padStart(2, 0))
+                }
+                setCurrentTime(hours + ':' + minutes)
             }
-        }
-        else if (hours > 13) {
-            if (minutes < 10) {
-                setCurrentTime(hours - 12 + ":" + minutes.padStart(2, 0))
-            }
-            else {
-                setCurrentTime(hours - 12 + ":" + minutes)
-            }
-        }
-        else {
-            if (minutes < 10) {
-                setCurrentTime(hours + ":" + minutes.padStart(2, 0))
-            }
-            setCurrentTime(hours + ':' + minutes)
-        }
-    });
+        }, 10000);
+        return () => clearInterval(interval)
+    })
 
-    return (
-        <div data-testid="time" className="date">Time: {currentTime}</div>
-    )
+    return (<div data-testid="time" className="date">Time: {currentTime}</div>)
+
 }
 
 export default Time;
